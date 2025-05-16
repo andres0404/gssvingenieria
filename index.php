b7f861f564006d433220ffd3e1251d008cba1129
Andres Silva Vega <elandrew2000@hotmail.com>
2025-05-16 10:41:06 -0500

    Agregar icono pestaña

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $objGeneral->consultar();
 <head>
 
     <meta charset="utf-8">
-    <link rel="icon" href="/img/logos/favicon.ico"> 
+    <link rel="icon" href="/img/logosClientes/favicon.ico"> 
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="<?php echo $objGeneral->get_desc_pagina(); ?>">
